2ce485f9c80e4ebcfc2700f5c4bfeda7b1f4bbb8
Jagdeep Singh <jdecode@gmail.com>
2020-06-15 21:48:32 +0530

    Removed ID from item, made default text gray-600(supporting dark-mode BG), reordered coloring of nav - reverse vibgyor (I think so)

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="w-40">
         <nav>
-            <TopNavItem name="Read" fillcolor="#2152B6" active="true"/>
-            <TopNavItem name="Learn" fillcolor="#AF3121"/>
-            <TopNavItem name="Discuss" fillcolor="#E87A2E"/>
-            <TopNavItem name="Listen" fillcolor="#32691A"/>
+            <TopNavItem name="Read" fillcolor="#AF3121" active="true"/>
+            <TopNavItem name="Learn" fillcolor="#E87A2E"/>
+            <TopNavItem name="Discuss" fillcolor="#32691A"/>
+            <TopNavItem name="Listen" fillcolor="#2152B6"/>
+            <TopNavItem name="Connect" fillcolor="#9400d3"/>
         </nav>
     </div>
 </template>
--- src/components/TopNavItem.vue
+++ src/components/TopNavItem.vue
@@ -1,8 +1,8 @@
 <template>
-    <a href="#" class="group flex items-center px-3 text-sm leading-5 font-medium text-gray-900 rounded-md focus:outline-none transition ease-in-out duration-150" aria-current="page">
+    <a href="#" class="group flex items-center px-3 text-sm leading-5 font-medium text-gray-600 rounded-md focus:outline-none transition ease-in-out duration-150" aria-current="page">
         <svg width="8" height="40" xmlns="http://www.w3.org/2000/svg">
             <g>
-                <rect stroke="#000" id="svg_1" height="40" width="8" y="0" x="0" stroke-width="0" :fill="fillcolor"/>
+                <rect stroke="#000" width="8" height="40" y="0" x="0" stroke-width="0" :fill="fillcolor"/>
             </g>
         </svg>
         <span class="truncate text-2xl pt-1 pl-1" :class="{'font-bold': active}">
