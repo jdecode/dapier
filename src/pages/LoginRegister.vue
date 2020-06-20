2d1b22d419ba2d5c254b77310afe9d3e75c4eb04
Jagdeep Singh <jdecode@gmail.com>
2020-06-20 22:10:42 +0530

    Add "Login with GitHub" button

--- src/pages/LoginRegister.vue
+++ src/pages/LoginRegister.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        Login/Register!
+    <div class="mr-64">
+        <div class="flex justify-center">
+            <span class="shadow-sm">
+                <a
+                    href="/app/github/oauth"
+                    type="button"
+                    class="
+                          inline-flex items-center px-6 py-3 text-base leading-6 font-medium
+                          text-gray-300 bg-black bg-opacity-75">
+                    <span class="text-4xl">
+                        <i class="fa fa-github"></i>
+                    </span>
+                    <span class="pl-2">Sign in with GitHub</span>
+                </a>
+            </span>
+        </div>
     </div>
 </template>>
 <script>
