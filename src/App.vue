e994833b55f6922f9d79cbf40e59c75fe37d415a
Jagdeep Singh <jdecode@gmail.com>
2020-06-20 22:47:23 +0530

    Add footer note - DAPIER <copyright> 2020 : default + VIBGYOR text (commented)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div
-      id="homepage"
       class="h-screen"
       :class="{'bg-gray-900 text-gray-500': this.dark, 'bg-gray-200 text-gray-700': !this.dark}">
-      <div class="flex mx-20">
-          <TopNav/>
-          <Dashboard/>
+      <div
+          id="homepage"
+          class="h-screen"
+      >
+          <div class="flex mx-20">
+              <TopNav/>
+              <Dashboard/>
+          </div>
       </div>
+      <footer class="flex-col text-center -mt-8">
+          <!--<span style="color: #9400d3">D</span><span style="color: #2152B6">API</span><span style="color: #32691A">ER</span><span style="color: #E87A2E">&copy;</span><span style="color: #AF3121">2020</span>-->
+          DAPIER &copy; 2020
+      </footer>
   </div>
 </template>
 
