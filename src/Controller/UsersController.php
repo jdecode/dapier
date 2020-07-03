d0a3078d59b77ecd886ba796988b604ec11cbd8c
Jagdeep Singh <jdecode@gmail.com>
2020-07-04 02:44:02 +0530

    Add "me" method - returns name and photo from user token

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,7 +11,6 @@ use Cake\Event\EventManagerInterface;
 use Cake\Http\Response;
 use Cake\Http\ServerRequest;
 use Cake\ORM\TableRegistry;
-use Cake\Utility\Text;
 
 /**
  * Users Controller
@@ -54,5 +53,58 @@ class UsersController extends AppController
         }
         echo json_encode(['logout' => $logout]);
         die;
+    }
+
+    public function me()
+    {
+        $token = $this->request->getHeader('Bearer')[0];
+
+        $user = [];
+
+        $eav = $this->marshaler->marshalJson('
+            {
+                ":token": "'.$token.'"
+            }
+        ');
+
+        $params = [
+            'TableName' => 'tokens',
+            'KeyConditionExpression' => '#token = :token',
+            'ExpressionAttributeNames'=> [ '#token' => 'token' ],
+            'ExpressionAttributeValues'=> $eav
+        ];
+
+        try {
+            $_token = $this->dynamoDb->query($params);
+            if (!$_token['Count'] || !$_token['Items'][0]['active']['BOOL']) {
+                $this->error('Unauthorized', 401);
+            }
+
+            $id_entity = $_token['Items'][0]['id_entity']['S'];
+            $eav = $this->marshaler->marshalJson('
+                    {
+                        ":id_entity": "'.$id_entity.'"
+                    }
+                ');
+
+            $params = [
+                'TableName' => 'users',
+                'IndexName' => 'sort_id_entity',
+                'KeyConditionExpression' => '#id_entity = :id_entity',
+                'ExpressionAttributeNames'=> [ '#id_entity' => 'id_entity' ],
+                'ExpressionAttributeValues'=> $eav
+            ];
+            $me = $this->dynamoDb->query($params);
+            if (!$me['Count'] || !$me['Items'][0]['active']['BOOL']) {
+                $this->error('Unauthorized', 401);
+            }
+            $user = [
+                'name' => $me['Items'][0]['github']['M']['name']['S'],
+                'photo' => $me['Items'][0]['github']['M']['photo']['S']
+            ];
+        } catch (DynamoDbException $DynamoDbException) {
+            $this->error($DynamoDbException->getMessage());
+        }
+        return $this->resp($user);
     }
 }
