8a2f95a154e7dbd2e1c4536c5d0306bf728a7633
Jagdeep Singh <jdecode@gmail.com>
2020-07-04 15:52:18 +0530

    Absolute path for favicon, remove long messages in absence of JS and Vue

--- templates/layout/vue-layout.php
+++ templates/layout/vue-layout.php
@@ -8,7 +8,7 @@ use Cake\Routing\Router;
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="/favicon.ico">
     <title>DAPIER</title>
     <link rel="stylesheet" href="<?= Router::url('/') ?>fonts/fontawesome-all.min.css" />
     <link rel="stylesheet" href="<?= Router::url('/') ?>fonts/font-awesome.min.css" />
@@ -17,15 +17,13 @@ use Cake\Routing\Router;
 <body>
     <noscript>
         <strong>
-            We're sorry but your browser doesn't support JavaScript. Please use an upgraded browser to continue.
+            !JS
         </strong>
     </noscript>
     <?= $this->Flash->render() ?>
     <?= $this->fetch('content') ?>
     <div id="app">
-        This is the root element "app".<br />
-        If Vue would be running fine, you wouldn't be seeing this message!<br />
-        Vue would use this element to load itself.
+        !V
     </div>
     <script type="text/javascript" defer src="<?= Router::url('/') ?>vue/js/chunk-vendors.js"></script>
     <script type="text/javascript" defer src="<?= Router::url('/') ?>vue/js/app.js"></script>
