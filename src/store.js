8c365d49add2adceb75bd91442b25d36d0471f63
Jagdeep Singh <jdecode@gmail.com>
2020-07-04 15:51:19 +0530

    Add auth, token and user params, and respective getters, mutations and actions

--- src/store.js
+++ src/store.js
@@ -6,7 +6,11 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         isDark: false,
-        token: ''
+        token: '',
+        auth: false,
+        user: {
+
+        }
     },
     getters: {
         DARK: state => {
@@ -14,17 +18,42 @@ export default new Vuex.Store({
         },
         TOKEN: state => {
             return state.token
-        }
+        },
+        AUTH: state => {
+            return state.auth
+        },
+        USER: state => {
+            return state.user
+        },
     },
     mutations: {
         THEME_UPDATE: ((state, isDark) => {
             localStorage.isDark = isDark;
             state.isDark = isDark;
+        }),
+        AUTH_UPDATE: ((state, auth) => {
+            state.auth = auth;
+        }),
+        TOKEN_UPDATE: ((state, token) => {
+            localStorage.token = token;
+            state.token = token;
+        }),
+        USER_UPDATE: ((state, user) => {
+            state.user = user;
         })
     },
     actions: {
         THEME_UPDATE: ((context, dark) => {
             context.commit('THEME_UPDATE', dark);
+        }),
+        AUTH_UPDATE: ((context, auth) => {
+            context.commit('AUTH_UPDATE', auth);
+        }),
+        USER_UPDATE: ((context, user) => {
+            context.commit('USER_UPDATE', user);
+        }),
+        TOKEN_UPDATE: ((context, token) => {
+            context.commit('TOKEN_UPDATE', token);
         })
     }
 });
