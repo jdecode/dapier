f169d731a9354a70cc9dde4e8fff7f00f4edc2bd
Jagdeep Singh <jdecode@gmail.com>
2020-07-04 15:50:30 +0530

    Use default CakePHP 4xx error file

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -1,38 +1,9 @@
 <?php
-/**
- * @var \App\View\AppView $this
- */
-use Cake\Core\Configure;
-use Cake\Error\Debugger;
 
-$this->layout = 'error';
+$this->layout = 'ajax';
 
-if (Configure::read('debug')) :
-    $this->layout = 'dev_error';
+echo json_encode([
+     'message' => $message,
+     'url' => $url
+ ], JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
 
-    $this->assign('title', $message);
-    $this->assign('templateName', 'error400.php');
-
-    $this->start('file');
-?>
-<?php if (!empty($error->queryString)) : ?>
-    <p class="notice">
-        <strong>SQL Query: </strong>
-        <?= h($error->queryString) ?>
-    </p>
-<?php endif; ?>
-<?php if (!empty($error->params)) : ?>
-        <strong>SQL Query Params: </strong>
-        <?php Debugger::dump($error->params) ?>
-<?php endif; ?>
-<?= $this->element('auto_table_warning') ?>
-<?php
-
-$this->end();
-endif;
-?>
-<h2><?= h($message) ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
-</p>
