5c100000cfcf27071068ad9a582b70352301ea27
Jagdeep Singh <jdecode@gmail.com>
2020-07-04 15:48:37 +0530

    Set user state, based on response of token-sending-API

--- src/main.js
+++ src/main.js
@@ -11,20 +11,35 @@ import App from './App.vue'
 
 let dapp = Vue.compile('<App></App>');
 
+import nivedan from 'nivedan';
+
 new Vue({
     el: '#app',
     store,
     data: {
+        token: ''
     },
     created ()
     {
-        this.checkToken();
     },
-    mounted() {
+    beforeMount() {
         this.setMode();
+        this.checkToken();
     },
     methods: {
         checkToken: function() {
+            this.setToken();
+            this.getUser();
+            this.clearTokenParam();
+        },
+        setMode: function () {
+            let lightMode = true;
+            if(typeof localStorage.isDark !== 'undefined') {
+                lightMode = JSON.parse(localStorage.isDark);
+            }
+            this.$store.dispatch("THEME_UPDATE", lightMode).then();
+        },
+        setToken: function () {
             let token = '';
             if(typeof this.$route.query.token === 'undefined') {
                 if (localStorage.token) {
@@ -34,23 +49,41 @@ new Vue({
                 token = this.$route.query.token;
             }
             if(token === '') {
-                localStorage.removeItem('token');
-                console.log('This should logout!');
-                //window.location.href = '/login';
+                this.logout();
             }
-            localStorage.token = token;
-            this.$store.state.token = token;
+            this.token = token;
+        },
+        logout: function () {
+            localStorage.removeItem('token');
+            console.log('Bye bye...!');
+            window.location.href = '/app/login';
+        },
+        getUser: async function () {
+            let nivedanToken = this.token;
+            nivedan.defaultConfig({
+                baseURL: '/api',
+                headers: {
+                    common: {
+                        'Bearer': nivedanToken
+                    },
+                }
+            });
+            try {
+                let user = {};
+                const response = await nivedan.get('/me');
+                user.name = response.data.name;
+                user.photo = response.data.photo;
+                this.$store.dispatch("TOKEN_UPDATE", this.token).then();
+                this.$store.dispatch("USER_UPDATE", user).then();
+                this.$store.dispatch("AUTH_UPDATE", true).then();
+            } catch (error) {
+                this.logout();
+            }
+        },
+        clearTokenParam: function () {
             if( typeof this.$route.query.token !== 'undefined' && this.$route.query.token.length > 0) {
                 this.$router.replace(this.$router.currentRoute.path).then();
             }
-        },
-        setMode: function () {
-            let lightMode = true;
-            if(typeof localStorage.isDark !== 'undefined') {
-                lightMode = JSON.parse(localStorage.isDark);
-            }
-            console.log(lightMode);
-            this.$store.dispatch("THEME_UPDATE", lightMode).then();
         }
     },
     render: dapp.render,
