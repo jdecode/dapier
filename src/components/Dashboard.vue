2ab379a2fc12ead78a10d53097b33e83a4e9b263
Jagdeep Singh <jdecode@gmail.com>
2020-07-08 12:24:09 +0530

    Remove borders

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,12 +2,12 @@
     <div class="w-9/12 z-10 mx-auto flex-row pt-1">
         <div>
             <ul class="flex flex-row-reverse">
-                <li v-if="auth" class="w-8 h-8 flex items-center border border-dashed cursor-pointer">
+                <li v-if="auth" class="w-8 h-8 flex items-center cursor-pointer">
                     <a href="/logout" class="text-2xl mx-auto" title="Logout">
                         <i class="fa fa-sign-out"></i>
                     </a>
                 </li>
-                <li class="mr-2 border w-8 h-8 border-dashed">
+                <li class="w-8 h-8 flex items-center cursor-pointer mx-4">
                     <router-link to="/login" class="text-2xl mx-auto" v-if="!auth">
                         <i class="fa fa-user"></i>
                     </router-link>
